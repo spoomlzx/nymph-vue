ccbf812b378613a5d364445d8c84552bda660ec6
spoomlan <spoomlzx@qq.com>
2018-10-28 18:26:40 +0800

    add store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCookie from 'vue-cookie'
+import './plugins/element.js'
 
+Vue.use(VueCookie);
 Vue.config.productionTip = false;
 
 new Vue({
